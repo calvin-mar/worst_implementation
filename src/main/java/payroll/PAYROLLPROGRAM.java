ab509db8c8beacbe34ee90aa420eddae2f1799d4
Jenna <jenna.nicospace@gmail.com>
2026-02-16 16:22:41 -0500

    Make variable names worse

--- src/main/java/payroll/PAYROLLPROGRAM.java
+++ src/main/java/payroll/PAYROLLPROGRAM.java
@@ -2,65 +2,65 @@ package payroll;
 
 public class PAYROLLPROGRAM {
 	
-	int[] paytype;						// Array of ints signifying employee type. 1 = Contractor, 2 = Hourly, 3 = Salary
-	String[] employeenames;				// Parallel array for names
-	int[] hours;						// Parallel array for hours worked
-	int[] payroll;						// Parallel array for amount of money they are paid
-	int index;							// Universal counter that tracks the arrays
-	int[] rate;						// Parallel array for RATE they are paid
+	int[] T;						// Array of ints signifying employee type. 1 = Contractor, 2 = Hourly, 3 = Salary
+	String[] N;				// Parallel array for names
+	int[] H;						// Parallel array for hours worked
+	int[] M;						// Parallel array for amount of money they are paid
+	int P;							// Universal counter that tracks the arrays
+	int[] R;						// Parallel array for RATE they are paid
 
 
 	// Constructor
 	public PAYROLLPROGRAM() {
-		this.paytype = new int[100];			// We are assuming (for some reason) that employees will not exceed 100
-		this.employeenames = new String[100];	
-		this.hours = new int[100];				
-		this.rate = new int[100];				
-		this.index = 0;							
+		this.T = new int[100];			// We are assuming (for some reason) that employees will not exceed 100
+		this.N = new String[100];	
+		this.H = new int[100];				
+		this.R = new int[100];				
+		this.P = 0;							
 	}
 	
 	// Add employee (modify four arrays...)
 	public void ADDEMPLOYEE(String name, int thetype) {
-		employeenames[index] = name;
-		paytype[index] = thetype;
-		hours[index] = 0;
-		rate[index] = 0;
-		index++;
+		N[P] = name;
+		T[P] = thetype;
+		H[P] = 0;
+		R[P] = 0;
+		P++;
 	}
 	
 	// Set employee hours
 	public void SETHOURS(int employeeindex, int thehours) {		// I guess the user is expected to know the index?
-		hours[employeeindex] = thehours;
+		H[employeeindex] = thehours;
 	}
 	
 	//Set employee rate
 	public void SETRATE(int employeeindex, int employeerate) {
-		rate[employeeindex] = employeerate;
+		R[employeeindex] = employeerate;
 	}
 	
 	
 	public int[] PAYEMPLOYEES() {
 		// Evil behemoth if statement goes here determining what to set the payroll values to.
-		for(int i=0; i<employeenames.length;i++) {
-			if(paytype[i] == 1) {
-				payroll[i]=hours[i]*rate[i];
-			}else if (paytype[i]==2) {
+		for(int i=0; i<N.length;i++) {
+			if(T[i] == 1) {
+				M[i]=H[i]*R[i];
+			}else if (T[i]==2) {
 				
-				if(hours[i] > 40) {
-					int hrsovertime = hours[i] - 40;
-					payroll[i]=(int) ((40*rate[i]) + (hrsovertime*(1.5*rate[i])));		
+				if(H[i] > 40) {
+					int hrsovertime = H[i] - 40;
+					M[i]=(int) ((40*R[i]) + (hrsovertime*(1.5*R[i])));		
 				}
 
-			}else if(paytype[i]==3) {
-				payroll[i]=40*rate[i];
+			}else if(T[i]==3) {
+				M[i]=40*R[i];
 
 			}
 			else {
-				payroll[i]=0;
+				M[i]=0;
 			}
 		}
 		// Returns the payroll
-		return payroll;
+		return M;
 	}
 	
 
